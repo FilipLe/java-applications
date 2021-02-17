2eff9c7d05f48bd24c0783eee6ddf11d24f7d5d7
nguyenle <nguyenle@nguyens-macbook-pro.home>
2021-02-17 01:00:11 +0100

    9:52

--- monarchs/src/monarchs/MonarchTableModel.java
+++ monarchs/src/monarchs/MonarchTableModel.java
@@ -3,7 +3,14 @@ package monarchs;
 import javax.swing.table.AbstractTableModel;
 
 public class MonarchTableModel extends AbstractTableModel{
-
+	//Creating Monarchs in a private array (people cannot change the data)
+	private Monarch[] monarchs = new Monarch[] {
+			//Format: Name: isMale : start of reign: end of reign
+			new Monarch("Henry VIII", true, 1509, 1547),
+			new Monarch("Mary I", false, 1553, 1558),
+			new Monarch("Elizabeth I", false, 1558, 1603)
+	};
+	
 	@Override
 	public int getRowCount() {
 		// TODO Auto-generated method stub
