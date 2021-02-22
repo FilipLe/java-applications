49f150670b97fa715bec0d964922e37af9092e62
nguyenle <nguyenle@nguyens-macbook-pro.home>
2021-02-22 10:44:51 +0100

    Part 4

--- monarchs/src/monarchs/MonarchTableModel.java
+++ monarchs/src/monarchs/MonarchTableModel.java
@@ -11,22 +11,88 @@ public class MonarchTableModel extends AbstractTableModel{
 			new Monarch("Elizabeth I", false, 1558, 1603)
 	};
 	
+	public String getColumnName(int columnIndex) 
+	{
+		//changing column headings
+		if(columnIndex == 0) 
+		{
+			return "Name";
+		}
+		else if(columnIndex == 1)
+		{
+			return "Gender";
+		}
+		else if(columnIndex == 2)
+		{
+			return "Start Year";
+		}
+		else if(columnIndex == 3)
+		{
+			return "End Year";
+		}
+		else 
+		{
+			return null;
+		}
+		
+	}
+	
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		//Returns length of array/number of rows
+		return monarchs.length;
 	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		// 4 columns: a column for name, gender, start year, and end year
+		return 4;
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		// Return the data that goes in each cell of the table
+		
+		// Row index to figure out which Monarch we are currently looking at
+		// Each Monarch will have their own row
+		Monarch monarch = monarchs[rowIndex];
+		
+		
+		//working out which piece of data about
+		if(columnIndex == 0) 
+		{
+			//Name is the first column
+			return monarch.getName();
+		}
+		else if(columnIndex == 1) 
+		{
+			//Gender column
+			
+			//Check if the monarch is a male and return their gender
+			if(monarch.getIsMale()) 
+			{
+				return "Male";
+			}
+			else 
+			{
+				return "Female";
+			}
+		}
+		else if(columnIndex == 2) 
+		{
+			//Start year
+			return monarch.getStartYear();
+		}
+		else if(columnIndex == 3) 
+		{
+			//End year
+			return monarch.getEndYear();
+		}
+		else 
+		{
+			return null;
+		}
+		
 	}
 
 }
