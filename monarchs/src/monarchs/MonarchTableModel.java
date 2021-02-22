df472fbfaf1c1ad676ea46b9b6c7ff7ddc04d2b7
nguyenle <nguyenle@nguyens-macbook-pro.home>
2021-02-22 21:19:24 +0100

    Vid 6: 26:22/32:25

--- monarchs/src/monarchs/MonarchTableModel.java
+++ monarchs/src/monarchs/MonarchTableModel.java
@@ -1,16 +1,34 @@
 package monarchs;
 
+import java.util.ArrayList;
+
 import javax.swing.table.AbstractTableModel;
 
 public class MonarchTableModel extends AbstractTableModel{
 	//Creating Monarchs in a private array (people cannot change the data)
-	private Monarch[] monarchs = new Monarch[] {
+	private Monarch[] presetMonarchs = new Monarch[] {
 			//Format: Name: isMale : start of reign: end of reign
-			new Monarch("Henry VIII", true, 1509, 1547, "https://englishhistory.net/wp-content/uploads/2017/03/henry8afterholbein3.jpg"),
-			new Monarch("Mary I", false, 1553, 1558, "https://englishhistory.net/images/tudor/elizsister.jpg"),
-			new Monarch("Elizabeth I", false, 1558, 1603, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Darnley_stage_3.jpg/1200px-Darnley_stage_3.jpg"),
-			new Monarch("Richard III", true, 1483, 1485, "https://cdn.britannica.com/47/182247-050-644E75E2/Richard-III-panel-artist.jpg")
+			new Monarch("Henry VIII", Gender.MALE, 1509, 1547, "https://englishhistory.net/wp-content/uploads/2017/03/henry8afterholbein3.jpg"),
+			new Monarch("Mary I", Gender.FEMALE, 1553, 1558, "https://englishhistory.net/images/tudor/elizsister.jpg"),
+			new Monarch("Elizabeth I", Gender.FEMALE, 1558, 1603, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Darnley_stage_3.jpg/1200px-Darnley_stage_3.jpg"),
+			new Monarch("Richard III", Gender.MALE, 1483, 1485, "https://cdn.britannica.com/47/182247-050-644E75E2/Richard-III-panel-artist.jpg")
 	};
+	
+	//arraylist to store the monarchs
+	//Arraylist is different from array that once an array is created, the size is fixed, but not in arraylists
+	private ArrayList<Monarch> monarchs = new ArrayList<>();
+	
+	public Monarch getMonarch(int rowIndex) {
+		return monarchs.get(rowIndex);
+	}
+	
+	//putting monarch into the table
+	public void addMonarch(Monarch monarch) {
+		//Adding monarch into the table
+		monarchs.add(monarch);
+		//updates table
+		fireTableDataChanged();
+	}
 	
 	public String getColumnName(int columnIndex) 
 	{
@@ -41,7 +59,7 @@ public class MonarchTableModel extends AbstractTableModel{
 	@Override
 	public int getRowCount() {
 		//Returns length of array/number of rows
-		return monarchs.length;
+		return monarchs.size();
 	}
 
 	@Override
@@ -56,7 +74,7 @@ public class MonarchTableModel extends AbstractTableModel{
 		
 		// Row index to figure out which Monarch we are currently looking at
 		// Each Monarch will have their own row
-		Monarch monarch = monarchs[rowIndex];
+		Monarch monarch = monarchs.get(rowIndex);
 		
 		
 		//working out which piece of data about
@@ -68,17 +86,8 @@ public class MonarchTableModel extends AbstractTableModel{
 		else if(columnIndex == 1) 
 		{
 			//Gender column
-			
-			//Check if the monarch is a male and return their gender
-			if(monarch.getIsMale()) 
-			{
-				return "Male";
-			}
-			else 
-			{
-				return "Female";
-			}
-		}
+			return monarch.getGender().toString();
+		} 
 		else if(columnIndex == 2) 
 		{
 			//Start year
@@ -94,9 +103,5 @@ public class MonarchTableModel extends AbstractTableModel{
 			return null;
 		}
 		
-	}
-	
-	Monarch getMonarch(int index) {
-		return monarchs[index];
 	}
 }
