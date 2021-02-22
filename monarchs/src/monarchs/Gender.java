50edf7f90daa502e399a96b279ef716c8ba2b1b2
nguyenle <nguyenle@nguyens-macbook-pro.home>
2021-02-22 21:19:08 +0100

    Vid 6: 26:22/32:25

--- monarchs/src/monarchs/Gender.java
+++ monarchs/src/monarchs/Gender.java
@@ -1,7 +1,21 @@
 package monarchs;
 
+//Class to store gender 
 public enum Gender {
-	UNKNOWN,
-	MALE,
-	FEMALE
+	UNKNOWN("Unknown"),
+	MALE("Male"),
+	FEMALE("Female")
+	;
+	
+	private String text;
+	
+	Gender(String text)
+	{
+		this.text = text;
+	}
+	
+	public String toString() 
+	{
+		return text;
+	}
 }
