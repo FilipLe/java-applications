45f76c9eabfb700ba61355714bc5fc5d208f9ab4
nguyenle <nguyenle@nguyens-macbook-pro.home>
2021-02-22 20:27:57 +0100

    P6

--- monarchs/src/monarchs/Portrait.java
+++ monarchs/src/monarchs/Portrait.java
@@ -2,7 +2,11 @@ package monarchs;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -10,16 +14,24 @@ import javax.imageio.ImageIO;
 public class Portrait {
 	private BufferedImage image;
 	
-	//Method to create portrait
-	public Portrait(String urlText) 
+	//Method to create portrait and save the portrait in the disk
+	public Portrait(File file, String urlText) 
 	{
-		//image equal to the portrait that we read from given URL
-		image = getPortrait(urlText);
+		//if that file does not exist, then download it
+		if(!file.exists()) 
+		{
+			//download file from provided URL
+			downloadFile(file,urlText);
+			
+			//download message, if image wasn't downloaded before
+			System.out.println("Downloading image from: " + urlText);
+		}
+		//loading file/portrait from hard disk into program
+		image = getPortrait(file);
 	}
 	
-	//Method to load image from the internet
-	public BufferedImage getPortrait(String urlText) 
-	{
+	//Method to download the file from internet and save it to our disk
+	public void downloadFile(File file, String urlText) {
 		try {
 			//creating new url from the URL text
 			URL url = new URL(urlText);
@@ -27,10 +39,36 @@ public class Portrait {
 			//open stream to read data from
 			InputStream stream = url.openStream();
 			
+			//reading raw data (array of bytes)
+			byte[] rawData = stream.readAllBytes();
+			
+			//close connection to the website
+			stream.close();
+			
+			//output stream which writes data to the file that we will provide as a parameter (L24)
+			OutputStream stream2 = new FileOutputStream(file);
+			
+			//write all that data to the stream
+			stream2.write(rawData);
+			stream2.close();
+		} catch (Exception e) {
+			//If an error occurs in any of the 4 statements above (in try{})
+			//then whatever the error is, we are going to crash the program
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//Method to load image from the file in disk
+	public BufferedImage getPortrait(File file) 
+	{
+		try {
+			//open stream to read data from
+			InputStream stream = new FileInputStream(file);
+			
 			//create image, reading data from that stream
 			BufferedImage image = ImageIO.read(stream);
 			
-			//close connection to the website
+			//close connection to disk
 			stream.close();
 			
 			//return the image that we created
