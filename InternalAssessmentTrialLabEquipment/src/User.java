75413c107272f8a2441ccbe42be94d506c5b1243
nguyenle <nguyenle@localhost>
2021-05-02 15:37:42 +0200

    17:05 JSON Video

--- InternalAssessmentTrialLabEquipment/src/User.java
+++ InternalAssessmentTrialLabEquipment/src/User.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+
+import com.github.cliftonlabs.json_simple.JsonObject;
 
 public class User {
 	private String name;
@@ -23,6 +26,28 @@ public class User {
 	public int getUserID()
 	{
 		return UserID;
+	}
+	
+	
+	//Converting to JSONObj()
+	public JsonObject toJsonObject() {
+		JsonObject jo = new JsonObject();
+		
+		//JSON format: <key>:<value> --> User's name
+		jo.put("name", name);
+		
+		//JSON format: <key>:<value> --> User's ID
+		jo.put("UserID", UserID);
+		
+		//return the JSON Object created
+		return jo;
+	}
+	
+	//This is going to take a Json Object and return a user with all the data stored in that object
+	public static User fromJsonObject(JsonObject jo) {
+		String name = (String)jo.get("name");
+		int UserID = ((BigDecimal)jo.get("UserID")).intValue();
+		return new User(name,UserID);
 	}
 }
 
