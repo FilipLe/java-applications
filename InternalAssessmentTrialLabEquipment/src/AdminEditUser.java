6e21541fb0b16d4f80fec5f6067f66aa7a5d01bd
nguyenle <nguyenle@localhost>
2021-05-03 09:55:37 +0200

    Now need to work on JSON in deleting/editing users + borrowing equipment

--- InternalAssessmentTrialLabEquipment/src/AdminEditUser.java
+++ InternalAssessmentTrialLabEquipment/src/AdminEditUser.java
@@ -3,19 +3,28 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.Font;
+import javax.swing.JPanel;
 
 public class AdminEditUser {
 
+	/*
+	 * GO TO Monarch App UI Main for the Listener thingy
+	 * + LINES 165-180 to continue editing my Listener thingy
+	 */
 	private JFrame frame;
 	private JTable table;
 	private UserTableModel tableModel;
+	private JTextField textFieldName;
+	private JTextField textFieldID;
 
 	/**
 	 * Launch the application.
@@ -44,7 +53,7 @@ public class AdminEditUser {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		frame = new JFrame("Equipment Management System");
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -53,14 +62,14 @@ public class AdminEditUser {
 		 * Edit button
 		 */
 		JButton btnEdit = new JButton("Edit");
-		btnEdit.setBounds(251, 26, 152, 29);
+		btnEdit.setBounds(276, 133, 152, 29);
 		frame.getContentPane().add(btnEdit);
 		
 		/*
 		 * Delete button
 		 */
 		JButton btnDelete = new JButton("Delete");
-		btnDelete.setBounds(251, 79, 152, 29);
+		btnDelete.setBounds(276, 174, 152, 29);
 		frame.getContentPane().add(btnDelete);
 		
 		/*
@@ -100,7 +109,74 @@ public class AdminEditUser {
 		 * Save Changes button
 		 */
 		JButton btnSaveChanges = new JButton("Save Changes");
-		btnSaveChanges.setBounds(251, 199, 152, 29);
+		btnSaveChanges.setBounds(276, 215, 152, 29);
 		frame.getContentPane().add(btnSaveChanges);
+		
+		/*
+		 * USER INFORMATION Title
+		 */
+		JLabel lblUserInformation = new JLabel("User Information:");
+		lblUserInformation.setFont(new Font("Lucida Grande", Font.BOLD | Font.ITALIC, 12));
+		lblUserInformation.setBounds(251, 7, 162, 16);
+		frame.getContentPane().add(lblUserInformation);
+		
+		
+		/*
+		 * USER'S NAME
+		 */
+		JLabel lblUserName = new JLabel("User Name:");
+		lblUserName.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lblUserName.setBounds(251, 43, 86, 16);
+		frame.getContentPane().add(lblUserName);
+		
+		textFieldName = new JTextField();
+		textFieldName.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		textFieldName.setColumns(10);
+		textFieldName.setBounds(317, 43, 111, 16);
+		frame.getContentPane().add(textFieldName);
+		
+		
+		/*
+		 * USER'S ID
+		 */
+		JLabel lblUserId = new JLabel("User ID:");
+		lblUserId.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lblUserId.setBounds(251, 87, 86, 16);
+		frame.getContentPane().add(lblUserId);
+		
+		textFieldID = new JTextField();
+		textFieldID.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		textFieldID.setColumns(10);
+		textFieldID.setBounds(298, 87, 130, 16);
+		frame.getContentPane().add(textFieldID);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(246, 6, 187, 116);
+		frame.getContentPane().add(panel);
+		
+		
+		
+		/*
+		 * Selecting user from the table to Edit or Delete
+		 */
+		table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent e) {
+				//Finding the row index of the row pressed
+				int rowIndex = table.getSelectedRow();
+				
+				//Ignore this value if this isn't the final value
+				//So it does the event below once, instead of twice when a row is clicked
+				//If the row index is greater than 0, then the selected row is valid
+				//And also if this particular selection is the final selection, then we print out the monarch
+				if(rowIndex >= 0 && !e.getValueIsAdjusting()) {
+					
+					//Get User at the clicked position
+					User user = tableModel.getUser(rowIndex);
+					
+					
+				}
+			}
+		});
 	}
 }
