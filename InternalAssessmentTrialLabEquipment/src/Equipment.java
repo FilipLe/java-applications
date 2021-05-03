801850d3455944e10fd67fc775d2aa68810d6c49
nguyenle <nguyenle@localhost>
2021-05-03 15:27:41 +0200

    Class for Equipments

---
+++ InternalAssessmentTrialLabEquipment/src/Equipment.java
@@ -0,0 +1,13 @@
+
+public class Equipment {
+	private String type;
+	private int ID;
+	private String status;
+	private String holderName;
+	private String holderID;
+	
+	//Constructor for Equipment
+	Equipment(String type, int ID, String status, String holderName, String holderID){
+		
+	}
+}
